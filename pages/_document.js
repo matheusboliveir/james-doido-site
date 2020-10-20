75b4cd875ef6a7e4cd1589b0d027a97d0793828a
Matheus Braz de Oliveira <theus.braz18@gmail.com>
2020-10-19 23:26:38 -0300

    google analytics config

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,15 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180941151-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-180941151-1');
+      </script>
+
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
             rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"></link>
